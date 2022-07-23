d92178692da1be87aa4057a4d5fec9b7524b9873
yyj <devyyj@gmail.com>
2022-07-23 23:27:57 +0900

    first push

---
+++ src/main/java/kr/yyj/pigdiary/PigdiaryApplication.java
@@ -0,0 +1,13 @@
+package kr.yyj.pigdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigdiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigdiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/yyj/pigdiary/PigdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yyj.pigdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigdiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
