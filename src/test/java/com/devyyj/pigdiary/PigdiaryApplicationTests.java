72fcbbf6df1965fc390b02b2deda6a66569683e2
devyyj <devyyj@gmail.com>
2023-05-12 13:37:06 +0900

    env 수정

--- src/test/java/com/devyyj/pigdiary/PigdiaryApplicationTests.java
+++ src/test/java/com/devyyj/pigdiary/PigdiaryApplicationTests.java
@@ -1,21 +1,8 @@
 package com.devyyj.pigdiary;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class PigdiaryApplicationTests {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Test
-    public void testEndoe(){
-        String password = "1111";
-        String enPw = passwordEncoder.encode(password);
-        System.out.println("enPw = " + enPw);
-        System.out.println("matches = " + passwordEncoder.matches(password, enPw));
-    }
 }
