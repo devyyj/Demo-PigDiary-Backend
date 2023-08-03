7e1ac34815dcb71d3e0d23daa8afa71db798fb32
devyy <devyyj@naver.com>
2023-08-03 21:50:51 +0900

    스프링 시큐리티 설정중

--- src/main/java/com/devyyj/pigdiary/config/SecurityConfig.java
+++ src/main/java/com/devyyj/pigdiary/config/SecurityConfig.java
@@ -1,24 +1,54 @@
 package com.devyyj.pigdiary.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@Log4j2
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/all").permitAll()
+                        .requestMatchers("/memeber").hasRole("USER")
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user1")
+                .password(passwordEncoder().encode("1111"))
+                .roles("USER")
+                .build();
+
+        log.info("userDetailsService............................");
+        log.info(user);
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/test/java/com/devyyj/pigdiary/PigdiaryApplicationTests.java
+++ src/test/java/com/devyyj/pigdiary/PigdiaryApplicationTests.java
@@ -1,13 +1,27 @@
 package com.devyyj.pigdiary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class PigdiaryApplicationTests {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
-    void test(){
-        System.out.println("한글");
+    public void testEncode() {
+        String pw = "1111";
+        String enpw = passwordEncoder.encode(pw);
+
+        System.out.println(enpw);
+
+        boolean matchResult = passwordEncoder.matches(pw, enpw);
+
+        System.out.println(matchResult);
     }
+
+
 }
