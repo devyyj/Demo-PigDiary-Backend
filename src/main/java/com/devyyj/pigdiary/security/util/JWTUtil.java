3124d2440dd7975fe3b67fff4cdc9a7ab786e725
devyyj <devyyj@gmail.com>
2023-08-09 16:37:06 +0900

    - JWT util 작성중

--- src/main/java/com/devyyj/pigdiary/security/util/JWTUtil.java
+++ src/main/java/com/devyyj/pigdiary/security/util/JWTUtil.java
@@ -2,10 +2,13 @@ package com.devyyj.pigdiary.security.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import io.jsonwebtoken.impl.DefaultJws;
 import lombok.extern.log4j.Log4j2;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
+
 
 @Log4j2
 public class JWTUtil {
@@ -16,8 +19,6 @@ public class JWTUtil {
     private long expire = 60 * 24* 30;
 
     public String generateToken(String content) throws Exception{
-
-
 
         return Jwts.builder()
                 .setIssuedAt(new Date())
@@ -33,7 +34,7 @@ public class JWTUtil {
         String contentValue = null;
 
         try {
-            String defaultJws = Jwts.parser()
+            DefaultJws defaultJws = (DefaultJws) Jwts.parser()
                     .setSigningKey(secretKey.getBytes("UTF-8")).parseClaimsJws(tokenStr);
 
             log.info(defaultJws);
